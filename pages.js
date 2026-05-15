60e50b515dfced05a6d0826ae3377f309dad1d52
mrrmegah <mrrmegah@gmail.com>
2026-05-15 13:01:02 +0200

    Add files via upload

--- pages.js
+++ pages.js
@@ -5,41 +5,42 @@ const stars = document.querySelectorAll(".star");
 const result = document.getElementById("result");
 const box = document.getElementById("ReviewMessage");
 
-
-links.forEach(link=>{
-    link.addEventListener("click", e =>{
+links.forEach(link => {
+    link.addEventListener("click", e => {
         e.preventDefault();
-
         document.querySelector(".load").classList.add("show");
-
-        setTimeout(()=>{
+        setTimeout(() => {
             window.location.href = link.href;
-        } , 400);
+        }, 400);
     });
 });
 
-btn.addEventListener("click", ()=>{
-    if(music.paused){
-        music.play();
-        btn.innerText = "Pause Music";
-    } 
-    else{
-        music.pause();
-        btn.innerText = "Play Music"
-    }
-});
+if (btn && music) {
+    btn.addEventListener("click", () => {
+        if (music.paused) {
+            music.play();
+            btn.innerText = "Pause Music";
+        } else {
+            music.pause();
+            btn.innerText = "Play Music";
+        }
+    });
+}
 
-function rate(num){
-    stars.forEach((star, index)=>{
-        if(index < num){
+function rate(num) {
+    stars.forEach((star, index) => {
+        if (index < num) {
             star.classList.add("active");
         } else {
             star.classList.remove("active");
         }
     });
-    result.innerText = num + "star rating"
+    if (result) result.innerText = num + " star rating";
 }
-box.addEventListener("input", ()=>{
-    box.style.height = "auto";
-    box.style.height = box.scrollHeight + "px";
-});
+
+if (box) {
+    box.addEventListener("input", () => {
+        box.style.height = "auto";
+        box.style.height = box.scrollHeight + "px";
+    });
+}
